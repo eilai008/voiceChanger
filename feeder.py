c9a1c892ebe3d34e3fd4ec58ceff189bdd1a9944
eilai008 <eilaidelorme0@gmail.com>
2026-03-22 23:03:51 +0200

    hi

---
+++ feeder.py
@@ -0,0 +1,27 @@
+import os
+
+import torch
+from torch.utils.data import Dataset
+
+
+class VoiceDataset(Dataset):
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self.data = []
+        fi=os.walk(data_path)
+        for x in fi:
+            for y in x[2]:
+                if not y.endswith(".pt"):
+                    continue
+                self.data.append(os.path.join(x[0], y))
+        self.len = len(self.data)
+    def __len__(self):
+        return self.len
+    def __getitem__(self, idx):
+        return torch.load(self.data[idx])
+
+
+def get_dataloader(dataset, config):
+    return torch.utils.data.DataLoader(dataset, batch_size=config["batch_size"], shuffle =True)
+
+
